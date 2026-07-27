e2257ebaf3c3d4950a56855ddf535c52e5533a56
Pavelius <p000000000001@gmail.com>
2026-07-27 19:55:35 +0300

    Flagable improved for native use. Use skill as native data struct.

--- bga/skill.h
+++ bga/skill.h
@@ -7,11 +7,12 @@ enum abilityn : unsigned char;
 
 enum skilln : unsigned char {
 	Alchemy, AnimalEmpathy, Bluff, Concentration, CraftWeapon, Diplomacy,
-	DisableDevice, Hide, Intimidate, KnowledgeArcana,
+	DisableDevice, Hide, Intimidate,
+	KnowledgeArcana, KnowledgeHistory, KnowledgeReligion,
 	MoveSilently, OpenLock, PickPocket, Search, Spellcraft, Spot,
 	UseMagicDevice, WildernessLore,
 };
-typedef flagable<1, unsigned> skillf;
+typedef flag32 skillf;
 struct skilli : nameable {
 	abilityn	ability;
 	skillf		synergy; // Get bonus +2 if skill have 5+ rang.
